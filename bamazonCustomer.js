745c433565567e2b0b192b8232e530436c7408d8
yelnats10 <yelnats10@gmail.com>
2017-11-10 20:00:08 -0500

    some conditions and mysql updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,11 +5,18 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
   
-    user: "eric",
+    user: "root",
   
-    password: "password",
+    password: "fzappaappazf",
     database: "bamazon"
   });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+   
+  });
+
 
 connection.query("SELECT * from products", function(err, rows){
   if(err){
@@ -17,12 +24,58 @@ connection.query("SELECT * from products", function(err, rows){
     return;
   }
   rows.forEach(function(result) {
-    console.log(result.item_id, result.product_name.replace(/['']+/g, ''), result.price);
+    console.log(result.item_id, result.product_name, result.price, result.stock_quantity);
   });
+  start();
 });
 
-  // connection.connect(function(err) {
-  //   if (err) throw err;
-  //   // run the start function after the connection is made to prompt the user
-  //   // start();
-  // });
+function start() {
+  inquirer.prompt ([{
+    name: "userPickID",
+    type: "input",
+    message: "What ID would you like to buy?"
+  },
+{
+  name: "userAmount",
+  type: "input",
+  message: "How many would you like to buy?"
+}])
+    .then(function(answer){
+      console.log(answer.userAmount);
+      console.log(answer.userPickID);
+      connection.query("SELECT * from products WHERE item_id =" + answer.userPickID, function(err,data){
+        console.log(data[0].stock_quantity);
+        if(data[0].stock_quantity < answer.userAmount){
+          console.log("Insufficient quantity!");
+        }else{
+          totalCost(answer.userPickID, answer.userAmount);
+        }
+      } )
+    })
+}
+
+function totalCost(ID, amount){
+  connection.query("SELECT * from products WHERE item_id =" + ID, function(err,data){
+    var total = amount * data[0].price;
+    console.log("$" + total.toFixed(2));
+    quantityUpdated(ID, amount, data[0].stock_quantity);
+  })
+  
+}
+
+function  quantityUpdated(item, stockAmount, quantity){
+
+  connection.query("UPDATE  products set ? where ?",
+  [
+    {
+      stock_quantity: quantity - stockAmount
+    },
+    {
+      item_id: item
+    }
+  ],
+  function(error,data){
+    if (error) throw error;
+    console.log("Updated successfully");
+  })
+}
